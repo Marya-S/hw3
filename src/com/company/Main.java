937e6eb141de2e07e26a0f5fc1967085f13e22d2
mariastepanova <stepanovams@sibur.ru>
2022-04-23 16:44:00 +0400

    Выполнено доп. задание

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class Main {
+public class Main<a> {
 
     public static void main(String[] args) {
         int osType = 0;
@@ -8,20 +8,17 @@ public class Main {
         System.out.println("****Задание 1 и 2*********");
         if (osType == 0 && produceYear >= 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        }
-        else if (osType == 0 && produceYear < 2015) {
+        } else if (osType == 0 && produceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        }
-        else if (osType == 1 && produceYear >= 2015) {
+        } else if (osType == 1 && produceYear >= 2015) {
             System.out.println("Установите версию приложения для Android по ссылке");
-        }
-        else if (osType == 1 && produceYear < 2015) {
+        } else if (osType == 1 && produceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         }
 
         System.out.println("****Задание 3*********");
         int year = 1600;
-        if (year % 4 == 0 && (!(year % 100 == 0) || (year%400==0))) {
+        if (year % 4 == 0 && (!(year % 100 == 0) || (year % 400 == 0))) {
             System.out.println(year + " год является високосным");
         } else {
             System.out.println(year + " год не является високосным");
@@ -32,14 +29,11 @@ public class Main {
         int day = 0;
         if (deliveryDistance < 20) {
             day += 1;
-        }
-        else if (deliveryDistance >= 20 && deliveryDistance < 60) {
+        } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
             day += 2;
-        }
-        else if (deliveryDistance >= 60 && deliveryDistance < 100) {
+        } else if (deliveryDistance >= 60 && deliveryDistance < 100) {
             day += 3;
-        }
-        else if (deliveryDistance >= 100) {
+        } else if (deliveryDistance >= 100) {
             day += 4;
         }
         System.out.println("Потребуется дней: " + day + " дней");
@@ -70,5 +64,47 @@ public class Main {
             default:
                 System.out.println("Вы ввели неверно месяц");
         }
+
+        System.out.println("****Задание 6*********");
+        int age = 19;
+        int salary = 65000;
+        int limit;
+
+        if (age >= 23) {
+            limit = 3 * salary;
+        } else {
+            limit = 2 * salary;
+        }
+        if (salary >= 50000 && salary < 80000) {
+            limit *= 1.2;
+        }
+        if (salary >= 80000) {
+            limit *= 1.5;
+        }
+        System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + limit + " рублей");
+        System.out.println("****Задание 7*********");
+        double baseRate = 10;
+        int loanTerm = 12;
+        int salary2 = 85000;
+        int wantedSum = 330000;
+        int age2 = 23;
+        double maxInstallment = 0.5 * salary2;
+
+        if (age2 < 30) {
+            baseRate += 0.5;
+        }
+        if (salary2 > 80000) {
+            baseRate -= 0.7;
+        }
+        double installment = (wantedSum * baseRate / 100 + wantedSum) / loanTerm;
+        if (installment <= maxInstallment) {
+            System.out.println("Максимальный платеж при ЗП " + salary2 + " равен " + maxInstallment + " рублей. " +
+                    "Платеж по кредиту " + installment + " рублей. Одобрено");
+        }
+        else{
+            System.out.println("Максимальный платеж при ЗП " + salary2 + " равен " + maxInstallment + " рублей. " +
+                    "Платеж по кредиту " + installment + " рублей. Отказано");
+        }
+
     }
 }
